d4c32dd385b1c8457c644a92c7201769420d4379
Tori <victoria.fafrak@gmail.com>
2019-02-15 20:39:58 -0500

    working game once through, does not repeat yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,8 @@ $("#wins").text(wins);
 $("#losses").text(losses);
 
 // generates random desired number player is trying to get to (19-120)
-var desiredNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+// 102 comes from: 120-19+1 from stack overflow answer (max-min+1)+min
+var desiredNumber = Math.floor(Math.random() * (102) + 19); 
 $("#num-goal").text(desiredNumber);
 
 var numberOptions = [];
@@ -18,15 +19,17 @@ $("#crystal-2").val(numberOptions[1]);
 $("#crystal-3").val(numberOptions[2]);
 $("#crystal-4").val(numberOptions[3]);
 
-console.log($("#crystal-1").val());
+// console.log($("#crystal-1").val());
 
 // sets player's current total to 0 and they will build on it
 var sum = 0;
 $("#total").text(sum);
 
+// function for when any crystal is clicked
 $(".crystal").on("click", function () {
     var crystalVal = $(this).val();
-    console.log(crystalVal);
+    // console.log(crystalVal);
+    crystalVal = parseInt(crystalVal);
     sum += crystalVal;
     $("#total").text(sum);
     console.log(sum);
@@ -43,78 +46,4 @@ $(".crystal").on("click", function () {
 });
 
 
-    // generates random desired number player is trying to get to (19-120)
-    // var desiredNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-    // $("#num-goal").text(desiredNumber);
-    // generates random numbers for each crystal (1-12)
-    // var crystalNum1 = Math.floor(Math.random() * 12) + 1
-    // $("#crystal-1").text(crystalNum1);
-    // var crystalNum2 = Math.floor(Math.random() * 12) + 1
-    // $("#crystal-2").text(crystalNum1);
-    // var crystalNum3 = Math.floor(Math.random() * 12) + 1
-    // $("#crystal-3").text(crystalNum1);
-    // var crystalNum4 = Math.floor(Math.random() * 12) + 1
-    // $("#crystal-4").text(crystalNum1);
-
-    // click commands for each crystal
-    // $("#crystal-1").on("click", function () {
-    //     sum += crystalNum1;
-    //     $("#total").text(sum);
-    //     console.log(sum);
-    //     if (sum === desiredNumber) {
-    //         wins++;
-    //         $("#wins").text(wins);
-    //         alert("You win!");
-    //     }
-    //     else if (sum > desiredNumber) {
-    //         losses++;
-    //         $("#losses").text(losses);
-    //         alert("You lose!");
-    //     }
-    // });
-    // $("#crystal-2").on("click", function () {
-    //     sum += crystalNum2;
-    //     $("#total").text(sum);
-    //     console.log(sum);
-    //     if (sum === desiredNumber) {
-    //         wins++;
-    //         $("#wins").text(wins);
-    //         alert("You win!");
-    //     }
-    //     else if (sum > desiredNumber) {
-    //         losses++;
-    //         $("#losses").text(losses);
-    //         alert("You lose!");
-    //     }
-    // });
-    // $("#crystal-3").on("click", function () {
-    //     sum += crystalNum3;
-    //     $("#total").text(sum);
-    //     console.log(sum);
-    //     if (sum === desiredNumber) {
-    //         wins++;
-    //         $("#wins").text(wins);
-    //         alert("You win!");
-    //     }
-    //     else if (sum > desiredNumber) {
-    //         losses++;
-    //         $("#losses").text(losses);
-    //         alert("You lose!");
-    //     }
-    // });
-    // $("#crystal-4").on("click", function () {
-    //     sum += crystalNum4;
-    //     $("#total").text(sum);
-    //     console.log(sum);
-    //     if (sum === desiredNumber) {
-    //         wins++;
-    //         $("#wins").text(wins);
-    //         alert("You win!");
-    //     }
-    //     else if (sum > desiredNumber) {
-    //         losses++;
-    //         $("#losses").text(losses);
-    //         alert("You lose!");
-    //     }
-    // });
-
+ 
